f794441a4bbcee9d281d70d3f43851c2961f3778
Aneeq <aneeqyounas28@gmail.com>
2021-09-02 11:12:34 -0700

    Initial commit

---
+++ aneeeq.py
@@ -0,0 +1,72 @@
+
+list1= ["aneeq younas ", "ameeque sarfraz", "sharoon rom", "anita kenneth", "jessica javeed", "khurram gill", "suniel khawar", "rameez rajput", "shan arif", "noel khan", "iqra anam", "vishal emmanuel", "afaq masih", "nauman nazir", "adnan shoukat" ]
+inp= input("Enter first two letters of your name: ")
+
+if inp==list1[0][0:2]:
+    print("Hello aneeq younas you are working at Salsh as a programmer JavaScript and C#.  interesting fact about you is you love to learn new things. your hobbies are Coding and programming.   Cooperative colleaguesIt makes you happy working at Salsh. Gaming make you feel  like your brain goes to it's highest potential when you are playing games.  Your mantra or motto of life is conquer your fears every day. ")
+
+elif inp==list1[1][0:2]:
+    print("Hello ameeque sarfraz, you are working at Salsh as a  project manager.  interesting fact about you, you  can hold conversations. your hobby is Crypto. your hobbies are  Interacting with people with diverse backgrounds excites me the most.  makes you happy working at Salsh is  Gaming enables me to live such moments which I would never have experienced on my own. your mantra or motto of life is  constant is Change! ")
+
+elif inp==list1[2][0:2]:
+    print("Hello sharoon rom you are founder and CEO of Salsh Games, currently you are working at FaceBook  as a junior game desginer. ")
+
+elif inp==list1[3][0:3]:
+    print("Hello anita kenneth you are working at Salsh as a  Creative Producer. You are an experienced marketeer over 15 years of experience in various fields. You are responsible for the Company’s marketing and communication strategies as well as overall branding and image. you held numerous events for promotion of various films (National and internationally), Pakistani culture and educating the young generation through media on culture and arts discovering new talents through various film festivals (BFI- British Film festival and EIFF- Edinburgh International film festival) contributing to cultural, social and economic development. You are  also an Industrial Pharmacist, Sales and marketing trainer with additional administrative and managerial skills. Experienced Radio broadcaster, Radio marketeer, Voice over artist, Cinema Operations Manager, Cinema Marketer, Googler and last but not the least Creative Producer SalshGames.  . Music keeps you happy.  For you working is a sacred place which keeps you happy while working. And at Salsh you find everything that keeps your soul fuelled up! Games, Music, Art, Stories, Voice Overs. And above all Salsh is an environment which is surely my sanctuary.  For you gaming is a state of mind which keeps me energised. It’s like my energy drink . Trust your crazy ideas! And feel the magic.  ")
+
+elif inp==list1[4][0:2]:
+    print("Hello jessica javeed ")
+
+elif inp==list1[5][0:2]:
+    print("Hello khuraam gill you are working at Salsh as a  (Chief Technology Officer). Award and significant accomplishment is Getting Salsh registered in Pakistan.  interesting fact about you is you never take selfie's. your hobbies are Watching series, playing video games, looking at new tech release videos on youtube. makes you happy working at Salsh is a huge deal for you, as you have got the people around you that are helping me groom professionally and to get this one of a kind opportunity is just surreal.Gaming just makes me kind of distant from everything. gaming make you feel like as if you are in another world where you can make anything happen.  Your mantra or motto of life is you Never judge others, Live in peace and let others live in peace too. ")
+
+elif inp==list1[6][0:2]:
+    print("Hello suniel khawar you are working at Salsh as a Video Game Composer. Awards and significant accomplishments are  Distinction as a Director for the Short Film “Makhboot Ul Hawas” (Thesis) – 2019 Represented Pakistan at International Univision Musical Festival in Baku, Azerbaijan – 2014 Performed at International Women Film Festival in Islamabad, Pakistan – 2017 Set up a professional independent Recording Studio  - Ambient Musicworks & Films Designed Music scores for the Short Films and Documentaries. River in the Desert  Maday Survival (ISPR) Safar Nama Music Box, Makhboot Ul Hawaas, Across the River. Your hobbies Are Since childhood, music was your hobby and later this hobby turned into my passion. Besides music, you spend your leisure time in watching documentaries because it helps to improve the depth of knowledge and a way to experience new worlds that you have never experience before. makes you happy working at Salsh is a platform where you can explore and execute your abilities in the Music Design. you feel glad and privileged when you create music scores of the video games as it is acquainted with your favorite genre. Your mantra or motto of life is to create Music that touches and connects to the souls of the individuals and leave a healing impression in their lives.  ")
+
+elif inp==list1[7][0:2]:
+    print("Hello rameez rajput you are working at Salsh as a Gobal Manager. interesting fact about you is you are a singer and a cook. your Hobbies are Sports, video games. makes you happy working at Salsh is where environment helps you to stay focus it also provides you happiness by sharing your thoughts and ideas through discussions with co-workers. gaming make you feel  is an alternate universe for your mind to exercise planning, making strategies and their execution. Your Mantra or Motto of Life is you always believed in “Power of Positivity” be positive, even in your darkest hour, and you shall see the light.   ")
+
+elif inp==list1[8][0:3]:
+    print("Hello shan arif you are working at Salsh as a assistant director Cinematics and motion capture. Working at Salsh for you  is life changing experience. Introduction to the new domain of entertainment and technology. For you gaming is catharsis. your mantra of life is There is always a positivity in every individual we just have to explore it. ")
+
+elif inp==list1[9][0:2]:
+    print("Hello noel khan you are working at Salsh as a 2D artist. Interesting fact about you is you are  vegetarian since i was 5 (i dont find this very interesting). your hobby is read comic books/manga, novels. Working at Salsh let's you do what you love as an artist which is to be creative and work on your skill and art, and also be able to make a living through it. Being a Pakistani citizen, jobs for video game artists are extremely rare and i feel blessed for this incredible opportunity. Video gaming ironically has been hard to do, given your financial conditions. But the few video games you have played, you have really enjoyed them, especially the ones that challenged you to keep on improving yourself. your motto in life is You are the stories you tell.  ")
+
+elif inp==list1[10][0:2]:
+    print("Hello iqra anam you are working at Salsh as a 2D Artist. - Award and significant accomplishment is  you won short film competition on child labor in 2016. You was given outstanding award for working at Arts Society 2020. The most interesting fact about you is that you can eat ice cream with teeth and you love to collect buttons. Your hobbies  are You are a nerd who loves to read, and write about my imaginations. makes you happy working at Salsh is super proud to work for your passion and Salsh has given you that opportunity. does gaming make you feel It seems like a elder sister to me because games were there when I needed them in my childhood. Your mantra or motto of life is Nothing is ever certain.   ")
+
+elif inp==list1[11][0:2]:
+    print("Hello vishal emmanuel you are working at Salsh as a Physical therapist. Award and significant accomplishment is Completed your research on Prevalence of imbalance and instability in geriatic population.  interesting fact about you is you are Experienced Physical Therapist with a demonstrated history of working in the health wellness and fitness industry. Skilled in Treatment Planning, Patient Care, Medicine, Home Care, and Occupational Therapy. Strong healthcare services professional with a Doctor of physical therapy focused in Sports and Exercise.   Your hobbies are Anatomy revision and Love to watch movies.   makes you happy working at Salsh is goes along with your happiness which is managing the medical emergencies and body physical requirements in pain recovery. gaming make you feel  kind of happiness on finishing the tasks or missions. Your mantra or motto of life is  never to quit every tomorrow is a second chnce never give up.    ")
+
+elif inp==list1[12][0:2]:
+    print("Hello afaq masih you are working at Salsh as a Machine learning engineer intern. Interesting fact about you is  one of your favorite things to do is observing nature and finding hidden patterns in  it.  you are  super excited to work with salsh games because you are  a fresh graduate, salsh games will give you a opportunity  to explore my knowledge in real life. gaming make you feel Think different AI ideas and applying these notions in real life through. your mantra and motto of life is Everyday try to learn new things.   ")
+
+elif inp==list1[13][0:2]:
+    print("Hello nauman nazir you are working at Salsh as a Legal consultant. Award and significant accomplishment is Haque versus Punjab Public Service Commission, in which the Punjab Information Commission 2021 and for that matter any court in Pakistan for the very first time held that evaluated answer scripts are information hence accessible by  an RTI application. Based on this ruling, the Governor of Punjab issued directions to all the universities of Punjab to go through a performance audit by providing evaluated answer scripts to everyone who applies.  perhaps nothing interesting about him. your hobbies are Playing cricket, coaching young cricketers, And a little bit of gardening.    ")
+
+elif inp==list1[14][0:2]:
+    print("Hello  adnan shoukat you are working at Salsh as a Project Manager.  Award and significant accomplishment is Launched Successful ED-Tech App (Edkasa)Black Belt in Taekwondo (Martial Arts).  interesting fact about you is you keep the people around you motivated and active by sharing my knowledge of fitness.  makes you happy working at Salsh is brining the Gaming Industry in Pakistan you will get the experience of working in Silicon Valley.  gaming make you feel relaxed and happy! . Your mantra or motto of life is Help people to achieve their fitness goals!  ")
+
+else:
+    print("SORRY!  We do not know you. ")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
